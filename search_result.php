89226c3f5cf39f481dd7eaefcf3780eb7890cc04
Huw Cocking <huw@cocking.id.au>
2026-05-24 12:06:46 +1000

    modified search feature to allow for job search

--- search_result.php
+++ search_result.php
@@ -4,42 +4,67 @@ require_once("settings.php");
 ?>
 <?php include "header.inc" ?>
 
-
 <?php
-# debugging values
-# $dbuser = "webuser";
-# $dbpass = "password123";
-$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
+$conn        = mysqli_connect($host, $dbuser, $dbpass, $dbname);
+$jobs        = [];
+$search_term = "";
+$error       = "";
 
-if (isset($_GET['search']) && !empty($_GET['search'])) {
-    $search_query = trim($_GET['search']);
+if (!$conn) {
+    $error = "Unable to connect to the database. Please try again later.";
+} elseif (isset($_GET['search']) && trim($_GET['search']) !== "") {
+    $search_term = trim($_GET['search']);
+    $search_safe = mysqli_real_escape_string($conn, $search_term);
 
-	  # Warning, unsanitised input. UNSAFE!
-	$query = "SELECT * FROM jobs WHERE title LIKE '%$search_query%'";
-	$result = mysqli_query($conn, $query);
-
-	$jobs = [];
-	while ($row = mysqli_fetch_assoc($result)) {
-		$jobs[] = $row;
-	}
+    $result = mysqli_query($conn, "SELECT * FROM jobs
+                                   WHERE title LIKE '%$search_safe%'
+                                      OR short_description LIKE '%$search_safe%'
+                                   ORDER BY id ASC");
+    if ($result) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $jobs[] = $row;
+        }
+    }
+    mysqli_close($conn);
 }
-
 ?>
 
 <main>
-<h3>Search results for <?php echo htmlspecialchars($search_query); ?>:</h3><br>
+    <section>
+        <h2 style="color: inherit; font-size: 2em; font-weight: bold;">Job Search Results</h2>
 
-<?php
-if (!empty($jobs)) {
-    echo "<ul>";
-    foreach ($jobs as $job) {
-        echo "<li>" . htmlspecialchars($job['title']) . "</li>";
-    }
-    echo "</ul>";
-} else {
-    echo "<p>No results found.</p>";
-}
-?>
+        <?php if ($error): ?>
+            <p><?php echo htmlspecialchars($error); ?></p>
 
+        <?php elseif ($search_term === ""): ?>
+            <p>Please enter a search term to find jobs.</p>
+
+        <?php elseif (empty($jobs)): ?>
+            <p>No jobs found matching <strong><?php echo htmlspecialchars($search_term); ?></strong>.</p>
+            <a href="jobs.php">View all available jobs</a>
+
+        <?php else: ?>
+            <p><?php echo count($jobs); ?> result(s) for <em><?php echo htmlspecialchars($search_term); ?></em>:</p>
+
+            <div class="job-cards-container">
+                <?php foreach ($jobs as $index => $job):
+                    $toggle_id        = "result-job" . ($index + 1) . "-toggle";
+                    $title            = htmlspecialchars($job['title']);
+                    $reference        = htmlspecialchars($job['reference']);
+                    $location         = htmlspecialchars($job['location']);
+                    $short_desc       = htmlspecialchars($job['short_description']);
+                    $salary           = number_format((int) $job['salary']);
+                    $reporting_line   = htmlspecialchars($job['reporting_line']);
+                    $responsibilities = explode('|', $job['key_responsibilities']);
+                    $essential        = explode('|', $job['essential_requirements']);
+                    $preferable       = explode('|', $job['preferable_requirements']);
+
+                    include "job_card.inc";
+                endforeach; ?>
+            </div>
+
+        <?php endif; ?>
+    </section>
 </main>
+
 <?php include "footer.inc" ?>
